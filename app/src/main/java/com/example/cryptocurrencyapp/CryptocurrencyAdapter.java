1c34a78f76de12b516f82684d9d36bd41f800e32
George Harder <harderg@oregonstate.edu>
2017-05-25 20:24:41 -0700

    add skeleton of files

---
+++ app/src/main/java/com/example/cryptocurrencyapp/CryptocurrencyAdapter.java
@@ -0,0 +1,8 @@
+package com.example.cryptocurrencyapp;
+
+/**
+ * Created by harderg on 5/25/17.
+ */
+
+public class CryptocurrencyAdapter {
+}
---
+++ app/src/main/java/com/example/cryptocurrencyapp/utils/CryptonatorUtils.java
@@ -0,0 +1,8 @@
+package com.example.cryptocurrencyapp.utils;
+
+/**
+ * Created by harderg on 5/25/17.
+ */
+
+public class CryptonatorUtils {
+}
